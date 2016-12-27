21dce8c7c0a506e51ebd164a4017e6167c696148
ZhangQz <zhang773575530@hotmail.com>
2016-12-27 17:58:56 +0000

    27/12
    MEnu hamburger

--- F/js/script.js
+++ F/js/script.js
@@ -1,13 +1,26 @@
 $(document).ready(function () {
+    $(".cross").hide();
+    $(".parar").hide();
     $(".icon_ham").click(function () {
         $("#menu").animate({
             height: 'toggle'
         });
+        $(".cross").show();
+        $(".icon_ham").hide();
     });
     $(".cross").click(function () {
-        $("#menu").slideToggle("slow", function () {
-            $(".cross").hide();
-            $(".icon_ham").show();
+        $("#menu").animate({
+            height: 'toggle'
         });
+        $(".cross").hide();
+        $(".icon_ham").show();
+    });
+    $(".ligar").click(function () {
+        $(".ligar").hide();
+        $(".parar").show();
+    });
+    $(".parar").click(function () {
+        $(".parar").hide();
+        $(".ligar").show();
     });
 });
