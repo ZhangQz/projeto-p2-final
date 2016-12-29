72f14054e6614eeb2eac4412544b84f64590cfe8
ZhangQz <zhang773575530@hotmail.com>
2016-12-29 00:39:30 +0000

    slide lateral do menu com testes de tablet

--- F/js/script.js
+++ F/js/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     $(".cross").hide();
     $(".parar").hide();
+    $(".info-content, .prog-content, .tecn-content, .cursos-content, .ruc-content").hide();
+    /*$(".inform").mouseover(function () {
+        $(".info-content").show();
+    });
+    $(".inform").mouseleave(function () {
+        $(".info-content").hide();
+    });*/
     $("#slideshow > div:gt(0)").hide();
     setInterval(function () {
         $('#slideshow > div:first')
@@ -12,17 +19,47 @@ $(document).ready(function () {
     }, 3000);
     $(".icon_ham").click(function () {
         $("#menu").animate({
-            height: 'toggle'
+            width: 'toggle'
         });
         $(".cross").show();
         $(".icon_ham").hide();
     });
     $(".cross").click(function () {
         $("#menu").animate({
-            height: 'toggle'
+            width: 'toggle'
         });
         $(".cross").hide();
         $(".icon_ham").show();
+    });
+    $(".inform").click(function () {
+        $(".info-content").animate({
+            height: 'toggle'
+        });
+        $(".prog-content, .tecn-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".programa").click(function () {
+        $(".prog-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .tecn-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".servi").click(function () {
+        $(".tecn-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".curs").click(function () {
+        $(".cursos-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .tecn-content, .ruc-content").hide();
+    });
+    $(".ru").click(function () {
+        $(".ruc-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .tecn-content, .cursos-content").hide();
     });
     $(".ligar").click(function () {
         $(".ligar").hide();
