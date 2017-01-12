713072605fd3c10772e92aebde0f0b6b91564a03
ZhangQz <zhang773575530@hotmail.com>
2017-01-12 18:15:02 +0000

    organização

---
+++ RUC/includes/index_inc.php
@@ -0,0 +1,58 @@
+
+            <div class="row">
+                <div class="filtro"></div>
+                <video muted autoplay loop id="pvid" alt="video apresentação">
+                <source src="css/Media/ruc.mp4" type="video/mp4">
+                <source src="css/Media/ruc_ogv.ogv" type="video/ogv">
+                 Your browser does not support the video tag.
+                </video>
+                <div class="col_3 colt_1"></div>
+                <div class="col_6 colt_6 colm_5 titulo">
+                    <p class="radiouni">Rádio Universidade de Coimbra <a class="centoesete">107.9FM</a></p>
+                </div>
+                <div class="col_3 colt_1"></div>
+            </div>
+            <div class="row gradiend"></div>
+            <section class="row preto">
+                <div class="col_2 colt_1"></div>
+                <div class="col_8 colt_6 colm_5 preto_slide">
+                    <div id="slideshow">
+                       <div>
+                         <img src="http://www.ruc.pt/wp-content/uploads/2016/11/14720594_10154256761858393_4201544313377765259_n-600x192.png" class="bacanal">
+                       </div>
+                       <div>
+                         <img src="http://www.ruc.pt/wp-content/uploads/2016/10/14330002_10154544568309530_1286348928535110057_n-600x192.jpg" class="bacanal_">
+                       </div>
+                       <div>
+                         <img src="http://www.ruc.pt/wp-content/uploads/2016/11/amen16-600x192.jpg" class="bacanal_1">
+                       </div>
+                    </div>
+                </div>
+                <div class="col_2 colt_1"></div>
+            </section>
+            <section class="row preto_">
+                <div class="col_2 colt_1"></div>
+                <div class="col_5 colt_4 colm_5">
+                    <div>
+                        <p class="ultimastitulo">Última Hora <a class="traco">-</a> <a>Informação</a></p>
+                    </div>
+                    <div class="newsfeed">
+                        <div class="new_content">
+                            <p class="content_title"><a>TEMA</a> / TITULO DA NOTICIA</p>
+                            <p>Ainda assim, existem dúvidas a respeito de como a adoção de políticas descentralizadoras causa impacto indireto na reavaliação dos paradigmas corporativos.</p>
+                        </div>
+                        <div class="new_content_">
+                            <p class="content_title"><a>TEMA</a> / TITULO DA NOTICIA</p>
+                            <p>Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't. When other websites give you text, they’re not sending the best. They’re not sending you, they’re sending words that have lots of problems and they’re bringing those problems with us. They’re bringing mistakes. They’re bringing misspellings. They’re typists… And some, I assume, are good wo</p>
+                        </div>
+                        <div class="new_content_1">
+                            <p class="content_title"><a>TEMA</a> / TITULO DA NOTICIA</p>
+                            <p>The concept of Lorem Ipsum was created by and for the Chinese in order to make U.S. design jobs non-competitive. Some people have an ability to write placeholder text... It's an art you're basically born with. You either have it or you don't. An 'extremely credible source' has called my office and told me that Lorem Ipsum's birth certificate is a fraud.</p>
+                        </div>
+                        <div class="ler_mais">
+                            <p>Ler mais</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col_2 colt_1"></div>
+            </section>
---
+++ RUC/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+    $(".cross").hide();
+    $(".parar").hide();
+    /*$(".inform").mouseover(function () {
+        $(".info-content").show();
+    });
+    $(".inform").mouseleave(function () {
+        $(".info-content").hide();
+    });*/
+   /* $("#seg, #ter, #qua, #qui, #sex, #sab, #dom").hide();*/
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(1000)
+            .next()
+            .fadeIn(1000)
+            .end()
+            .appendTo('#slideshow');
+    }, 3000);
+    $(".icon_ham").click(function () {
+        $("#menu").animate({
+            width: 'toggle'
+        });
+        $(".cross").show();
+        $(".icon_ham").hide();
+    });
+    $(".cross").click(function () {
+        $("#menu").animate({
+            width: 'toggle'
+        });
+        $(".cross").hide();
+        $(".icon_ham").show();
+    });
+    $(".inform").click(function () {
+        $(".info-content").animate({
+            height: 'toggle'
+        });
+        $(".prog-content, .tecn-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".programa").click(function () {
+        $(".prog-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .tecn-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".servi").click(function () {
+        $(".tecn-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .cursos-content, .ruc-content").hide();
+    });
+    $(".curs").click(function () {
+        $(".cursos-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .tecn-content, .ruc-content").hide();
+    });
+    $(".ru").click(function () {
+        $(".ruc-content").animate({
+            height: 'toggle'
+        });
+        $(".info-content, .prog-content, .tecn-content, .cursos-content").hide();
+    });
+    $(".ligar").click(function () {
+        $(".ligar").hide();
+        $(".parar").show();
+    });
+    $(".parar").click(function () {
+        $(".parar").hide();
+        $(".ligar").show();
+    });
+    $(".acesso").click(function () {
+        $(".sign_in").toggle("slide");
+    });
+    $(".exit").click(function () {
+        $(".sign_in").toggle("slide");
+        $(".log_in_cont").toggle("slide");
+    });
+    $(".login").click(function () {
+        $(".log_in_cont").toggle("slide");
+        $(".sign_in").hide("slide");
+    });
+    $(".user_icon").click(function () {
+        $(".user_cont").toggle("slide");
+    });
+});
