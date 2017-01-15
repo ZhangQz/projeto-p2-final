7ff187729fc0ebdac556500cf89991f7bcabec4a
Carolinahc <carolinahenriquescastro@MBPdeCarolina2.lan>
2017-01-15 22:23:41 +0000

    22.20

---
+++ RUC/slideshow.inc.php
@@ -0,0 +1,30 @@
+ <div class="row ploco">
+                <div class="filtro"></div>
+                <video muted autoplay loop id="pvid" alt="video apresentação">
+                <source src="assets/media/ruc.mp4" type="video/mp4">
+                 Your browser does not support the video tag.
+                </video>
+                <div class="col_3 colt_1"></div>
+                <div class="col_6 colt_6 colm_5 titulo">
+                    <p class="radiouni">Rádio Universidade de Coimbra <a class="centoesete">107.9FM</a></p>
+                </div>
+                <div class="col_3 colt_1"></div>
+            </div>
+            <div class="row gradiend"></div>
+            <section class="row preto">
+                <div class="col_2 colt_1"></div>
+                <div class="col_8 colt_6 colm_5 preto_slide">
+                    <div id="slideshow">
+                       <div>
+                         <img src="assets/Images/slide_1" class="bacanal">
+                       </div>
+                       <div>
+                         <img src="http://www.ruc.pt/wp-content/uploads/2016/10/14330002_10154544568309530_1286348928535110057_n-600x192.jpg" class="bacanal_">
+                       </div>
+                       <div>
+                         <img src="http://www.ruc.pt/wp-content/uploads/2016/11/amen16-600x192.jpg" class="bacanal_1">
+                       </div>
+                    </div>
+                </div> 
+               <div class="col_2 colt_1"></div>
+            </section>
