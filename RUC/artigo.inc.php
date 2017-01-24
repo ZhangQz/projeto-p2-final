af35dc9bbda7b88f7b5bfc303d3bb2bc97aa3e50
ZhangQz <zhang773575530@hotmail.com>
2017-01-24 23:34:25 +0000

    s

---
+++ RUC/artigo.inc.php
@@ -0,0 +1,12 @@
+<div class="col_5 colt_4 colm_5">
+    <div>
+        <p class="ultimastitulo">Última Hora <a class="traco">-</a> <a>Informação</a></p>
+    </div>
+    <div class="newsfeed">
+        <?php
+        foreach ($row = $produtos->fetch(PDO::FETCH_ASSOC))
+        echo <div class=\"new_content\">
+            <p class=\"content_title\"><a>/*Include da categoria*/</a>/*Include do titulo*/</p>
+            <p>/*Include do conteudo*/</p>
+        </div>?>
+    </div>
